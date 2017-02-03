34b4fe15a380d5b8a527905554b6f5739b0b81b6
Delfin Abrencillo <delfin-jr.p.abrencillo@hp.com>
2017-02-03 15:32:49 -0800

    Add python function for solving LCM.

---
+++ python2/algorithm/euclid.py
@@ -0,0 +1,20 @@
+import sys
+
+def gcd(a, b):
+    '''
+    Find the greatest common denominator of two numbers.
+    '''
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+def lcm(a, b):
+    '''
+    Find the least common multiple of two numbers.
+    '''
+    return (a * b) / gcd(b, a % b)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        print 'The GCD of {} and {} is {}.'.format(sys.argv[1], sys.argv[2], gcd(int(sys.argv[1]), int(sys.argv[2])))
+        print 'The LCM of {} and {} is {}.'.format(sys.argv[1], sys.argv[2], lcm(int(sys.argv[1]), int(sys.argv[2])))
